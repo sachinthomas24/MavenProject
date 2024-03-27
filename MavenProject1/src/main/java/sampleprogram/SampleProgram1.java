c5e3214276095c4584b41abe08579603d92356c4
ADMIN <ADMIN@DESKTOP-N01GR5P.bbrouter>
2024-03-27 20:33:54 +0530

    Sample

---
+++ MavenProject1/src/main/java/sampleprogram/SampleProgram1.java
@@ -0,0 +1,15 @@
+package sampleprogram;
+
+public class SampleProgram1 {
+	public void display()
+	{
+		System.out.println("Hello World");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SampleProgram1 obj = new SampleProgram1();
+		obj.display();
+	}
+
+}
