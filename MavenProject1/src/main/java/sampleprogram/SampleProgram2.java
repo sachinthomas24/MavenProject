a771755eaa921522fd55a934a9c8861ff1343981
ADMIN <ADMIN@DESKTOP-N01GR5P.bbrouter>
2024-03-27 20:59:41 +0530

    Commit Msg

--- MavenProject1/src/main/java/sampleprogram/SampleProgram2.java
+++ MavenProject1/src/main/java/sampleprogram/SampleProgram2.java
@@ -1,7 +1,17 @@
 package sampleprogram;
 
 public class SampleProgram2 {
-
+	int sum;
+	public void add(int a, int b)
+	{
+		this.print();
+		sum = a+b;
+		System.out.println("Sum of numbers ="+sum);
+	}
+	public void print()
+	{
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
